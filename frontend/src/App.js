3bdd61063a382d8e26cd93dd899a1d811140c4cd
santushtisharma10 <sharmasantushti1012@gmail.com>
2022-01-23 17:13:15 +0530

    frontend folder added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <i className='fas fa-shopping-cart'></i>
+    </div>
+  );
+}
+
+export default App;
