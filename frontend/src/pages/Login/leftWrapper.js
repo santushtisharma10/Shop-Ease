532ee4ed74c4c46a9de5b68497ef574d2c9c4b7e
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-05-06 16:16:32 +0530

    [modification]: fade animation added

--- frontend/src/pages/Login/leftWrapper.js
+++ frontend/src/pages/Login/leftWrapper.js
@@ -1,43 +1,48 @@
 import React from "react";
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-import {Autoplay} from 'swiper'
-import img1 from '../../images/online-shopping.jpg';
-import img2 from '../../images/shop1.jpg';
-
+//library
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade } from "swiper";
+//css
+import "swiper/css/effect-fade";
 import "swiper/css";
+//images
+import img1 from "../../images/online-shopping.jpg";
+import img2 from "../../images/shop1.jpg";
 
-//fade animation for slides
 //image fit
 //add 3 images
 
 function LeftWrapper() {
   const imagesArray = [
     {
-      id: '1',
+      id: "1",
       href: img1,
     },
     {
-      id: '2',
+      id: "2",
       href: img2,
+    },
+    { id: "3", 
+      href: img1
     }
-  ]
+  ];
 
   return (
     <div style={{ backgroundColor: "pink", width: "50vw", height: "100vh" }}>
       <Swiper
-         spaceBetween={30}
- 
-  autoplay= {{
-      delay: 2500,
-      disableOnInteraction: false,
-  }}
-        
-         modules={[Autoplay]}
+        spaceBetween={30}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        effect="fade"
+        modules={[Autoplay, EffectFade]}
       >
-      
-        {imagesArray.map((img) => <SwiperSlide id={img.id}><img style={{width: '100%'}} src={img.href}/></SwiperSlide>)}
-        
+        {imagesArray.map((img) => (
+          <SwiperSlide key={img.id}>
+            <img style={{ width: "100%" }} src={img.href} alt={img.id}/>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
