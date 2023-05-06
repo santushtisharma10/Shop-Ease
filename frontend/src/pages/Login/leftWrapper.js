62bc8baf7dfa9bc400a14aad820c17f64f345018
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-05-07 01:46:40 +0530

    [modification]: changes made

--- frontend/src/pages/Login/leftWrapper.js
+++ frontend/src/pages/Login/leftWrapper.js
@@ -1,4 +1,6 @@
 import React from "react";
+//@mui
+import { styled } from "@mui/material";
 //library
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade } from "swiper";
@@ -8,39 +10,39 @@ import "swiper/css";
 //images
 import img1 from "../../images/online-shopping.jpg";
 import img2 from "../../images/shop1.jpg";
+import img3 from "../../images/shop1.jpg";
 
-//image fit
-//add 3 images
+const Image = styled("img")({
+  objectFit: "fill",
+  width: "100%",
+  height: "100%",
+});
 
 function LeftWrapper() {
   const imagesArray = [
-    {
-      id: "1",
-      href: img1,
-    },
-    {
-      id: "2",
-      href: img2,
-    },
-    { id: "3", 
-      href: img1
-    }
+    { id: "1", href: img1 },
+    { id: "2", href: img2 },
+    { id: "3", href: img3 },
   ];
 
   return (
-    <div style={{ backgroundColor: "pink", width: "50vw", height: "100vh" }}>
+    <div style={{ width: "50%", height: "100vh" }}>
       <Swiper
         spaceBetween={30}
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
         }}
+        style={{ height: "100%" }}
         effect="fade"
         modules={[Autoplay, EffectFade]}
       >
         {imagesArray.map((img) => (
           <SwiperSlide key={img.id}>
-            <img style={{ width: "100%" }} src={img.href} alt={img.id}/>
+            <Image
+              src={img.href}
+              alt={img.id}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
